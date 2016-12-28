1bba5ce2c859d0762c4a746163c1a75050403ea9
Nate Tangsurat <e4r7hbug@gmail.com>
2016-12-28 15:04:44 -0600

    style: Sort format list

--- src/gogoutils/formats.py
+++ src/gogoutils/formats.py
@@ -13,7 +13,6 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
 """Determine the generator format"""
 try:
     from collections import ChainMap
@@ -21,32 +20,31 @@ except ImportError:
     from ConfigParser import _Chainmap as ChainMap
 
 DEFAULT_FORMAT = {
-    'domain': 'example.com',
+    'apigateway_domain': 'api.{env}.{domain}',
     'app': '{repo}{project}',
-    'dns_elb': '{repo}.{project}.{env}.{domain}',
     'dns_elb_region': '{repo}.{project}.{region}.{env}.{domain}',
+    'dns_elb': '{repo}.{project}.{env}.{domain}',
     'dns_global': '{repo}.{project}.{env}.{domain}',
     'dns_instance': '{repo}{project}-xx.{env}.{domain}',
+    'domain': 'example.com',
+    'git_repo_configs': '{raw_project}/{raw_repo}-config',
+    'git_repo_qe': '{raw_project}/{raw_repo}-qa',
+    'git_repo': '{raw_project}/{raw_repo}',
     'iam_base': '{project}_{repo}',
-    'iam_user': '{project}_{repo}',
     'iam_group': '{project}',
-    'iam_role': '{project}_{repo}_role',
     'iam_policy': '{project}_{repo}_policy',
     'iam_profile': '{project}_{repo}_profile',
-    's3_bucket': 'archaius-{env}',
-    's3_app_bucket': '{project}-{repo}',
-    's3_bucket_path': '{project}/{repo}{project}',
-    's3_archaius_name': 'archaius-{env}/{project}/{repo}{project}/',
+    'iam_role': '{project}_{repo}_role',
+    'iam_user': '{project}_{repo}',
     'jenkins_job_name': '{project}_{repo}',
-    'git_repo': '{raw_project}/{raw_repo}',
-    'git_repo_qe': '{raw_project}/{raw_repo}-qa',
-    'git_repo_configs': '{raw_project}/{raw_repo}-config',
-    'apigateway_domain': 'api.{env}.{domain}',
+    's3_app_bucket': '{project}-{repo}',
+    's3_archaius_name': 'archaius-{env}/{project}/{repo}{project}/',
+    's3_bucket_path': '{project}/{repo}{project}',
+    's3_bucket': 'archaius-{env}',
 }
 
 
 class Formats(object):
-
     def __init__(self, config={}):
         self.config = ChainMap(config, DEFAULT_FORMAT)
 
